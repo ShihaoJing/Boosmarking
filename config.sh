514e45cda6052958b8bb3b6d2f21b0a0d73d8b94
Shihao Jing <shihao.jing810@gmail.com>
2017-04-01 10:55:49 -0400

    add config.sh for cross compile

--- config.sh
+++ config.sh
@@ -1,6 +1,14 @@
-#!/bin/zsh
+#!/bin/bash
 
+if [ -d build ]; then
+  rm -rf build
+fi
 mkdir build
 cd build
-cmake -DOPENSSL_ROOT_DIR=$(brew --prefix openssl) -DOPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include" ..
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        cmake ..
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+        cmake -DOPENSSL_ROOT_DIR=$(brew --prefix openssl) -DOPENSSL_INCLUDE_DIR=$(brew --prefix openssl)"/include" ..
+fi
 make
